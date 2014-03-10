c8eb6dc0c278f00995fcba3c627a908d1b8dc92d
nadya williams <nadya@sdsc.edu>
2014-03-10 14:47:23 -0700

    update build calls with correct names

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,35 +1,39 @@
 #!/bin/bash
-
-# Create prerequisites for building  lmserver dependencies
-
+#
+# Build and install prerequisites for compiling lmserver dependencies
+#
 . /opt/rocks/share/devel/src/roll/etc/bootstrap-functions.sh
 
-# add pgdg repo: need for installing postgresql and postgis2 rpms
+# yum repo for postgresql and postgis2 rpms
 (cd src/RPMS; rpm -i pgdg-centos91-9.1-4.noarch.rpm)
 
-# for mapserver
+# yum repo and vera fonts for mapserver
 yum --enablerepo base install cmake
-(cd src/RPMS; \
-	rpm -i elgis-release-6-6_0.noarch.rpm; \
-	rpm -i bitstream-vera-fonts-common-1.10-18.el6.noarch.rpm; \
-	rpm -i bitstream-vera-sans-fonts-1.10-18.el6.noarch.rpm;\
-)
+rpm -i src/RPMS/elgis-release-6-6_0.noarch.rpm 
+rpm -i src/RPMS/bitstream-vera-fonts-common-1.10-18.el6.noarch.rpm
+rpm -i src/RPMS/bitstream-vera-sans-fonts-1.10-18.el6.noarch.rpm
 echo "/usr/java/latest/jre/lib/amd64" > /etc/ld.so.conf.d/lifemapper-server.conf
 echo "/usr/java/latest/jre/lib/amd64/server" >> /etc/ld.so.conf.d/lifemapper-server.conf
 /sbin/ldconfig
 
-# for building PyLucene 
-compile_and_install ant
+# for PyLucene 
+compile ant
+install lifemapper-ant
 export ANT_HOME=/opt/lifemapper
 (cd src/pylucene;  make install_jcc)
 
-# for building pytables 
-compile_and_install cython 
-compile_and_install numexpr 
+# for pytables 
+compile cython 
+install opt-cython 
+compile numexpr 
+install opt-numexpr 
 yum --enablerepo rpmforge install hdf5 hdf5-devel
 
-# for building psycopg
-compile_and_install gdal
+# for psycopg2
+compile gdal
+install lifemapper-gdal
 /sbin/ldconfig
+
+# install postgresql91
 yum install postgresql91
 
