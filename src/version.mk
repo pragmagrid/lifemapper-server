79e1d8dbc3f8149c8e0b629508dd633ea47c1c4f
nadyawilliams <nadya@sdsc.edu>
2014-03-28 10:22:02 -0700

    use template for lmconstants.py

--- src/version.mk
+++ src/version.mk
@@ -2,5 +2,7 @@ PKGROOT = /opt/lifemapper
 LMHOME  = /opt/lifemapper/lmserver
 MAPSERVER_TMP = /var/www/tmp
 LMWEB = /var/lib/lm2
+DATADIR  = /share/lmserver/data
+TEMPDIR  = /tmp
 PYTHONVER = python2.7
 PGSQL = /usr/pgsql-9.1/bin
