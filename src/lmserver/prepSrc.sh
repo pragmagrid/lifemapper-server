3c9c217dd8d248ac9cb8a8c843319b45453cad25
nadyawilliams <nadya@sdsc.edu>
2014-07-28 10:42:47 -0700

    use svn revision as distro identifier

--- src/lmserver/prepSrc.sh
+++ src/lmserver/prepSrc.sh
@@ -1,53 +1,60 @@
 #!/bin/bash
 
-# Purpose: checkout src/ from lifemapper SVN and create distro tar.gz file to use in RPM. 
-# Need to pass svn url as a first argument. 
-# Will be prompted for svn user and passwd.
+# Purpose: checkout distro from lifemapper SVN and create *.tar.gz files to use in RPM. 
+# NOTE: Will be prompted for valid svn user and passwd.
 
 SRC=components
 URL=https://svn.lifemapper.org/trunk
-echo "NOTE: for svn checkout you will be prompted for valid user/pass "
 
 # get src distro from lifemapper svn
-svnCheckout () {
+checkout_svn () {
       echo "Starting SVN checkout from $1:"
-      svn checkout $1/components
+      svn checkout $1/$SRC
+
+      echo "Record SVN revision for $SRC:"
+      svn info $SRC > $SRC/svn-info
+      REV=`svn info $SRC | grep Revision | awk '{print $2}'`
+
+      echo "Removing .svn in $SRC:"
       if [ -d $SRC ]; then
           DIRS=`find $SRC -name .svn`
           for i in $DIRS; do
-              echo "removing $i"
               rm -rf $i
           done
       else
-          echo "Error with SVN checkout: directory $SRC is not created"
+          echo "Error with SVN checkout from $URL/$SRC: directory $SRC is not created"
       fi
 }
 
-mvSrc () {
-  DIRS="LmCompute LmDbServer LmWebServer LmServerCommon LmCommon"
+# move src/* in all modules one level up
+collect_src () {
+  DIRS=`find $SRC -maxdepth 1 -type d`
   for i in $DIRS; do
-    dirname=$SRC/$i
-    if [ -d $dirname/src ] ; then
-      echo "moving files from $dirname/src"
-      mv  $dirname/src/* $dirname
-      rm -rf $dirname/src/
-    else
-      echo "directory $dirname/src is not present"
+    if [ -d $i/src ] ; then
+      echo "Moving files from $i/src/ to $i"
+      mv  $i/src/* $i
+      rm -rf $i/src/
     fi
   done
 }
 
-# create distro file
-compressFiles () {
+# create distro files for lmserver and lmcompute
+create_distro () {
   if [ -d $SRC ]; then
-      echo "Creating src archive from svn checkout"
-      DATE=`date +%Y%m%d`
-      tar czvf src-$DATE.tar.gz $SRC/* --exclude=LmCompute
+      #DATE=`date +%Y%m%d`
+      # create lmserver src distro
+      echo "Creating lmserver src archive from svn checkout"
+      tar czf lifemapper-server-$REV.tar.gz $SRC/* --exclude=LmCompute --exclude=dist --exclude=public_html/dl
+
+      # create lmcompute src distro
+      echo "Creating lmcompute src archive from svn checkout"
+      tar czf lifemapper-lmcompute-$REV.tar.gz $SRC/LmCompute $SRC/LmCommon $SRC/config
   else
-      echo "Svn checkout directory $SRC is not present"
+      echo "SVN checkout directory $SRC is not present"
   fi
 }
 
-svnCheckout $URL
-mvSrc
-compressFiles
+### main ###
+checkout_svn $URL
+collect_src
+create_distro
