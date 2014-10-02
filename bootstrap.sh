bf4e3e10c79179c9a1d8eceffb6fcd1c6dc84aa6
root <root@fe.public>
2014-10-02 11:51:02 -0700

    add all dependent rpms to distro - have known version available

--- bootstrap.sh
+++ bootstrap.sh
@@ -18,30 +18,32 @@ wget http://yum.postgresql.org/9.1/redhat/rhel-6-x86_64/$PGDGREPO
 rpm -i $PGDGREPO
 )
 
-# download needed RPMS
-(cd src/RPMS; 
-yumdownloader --resolve --enablerepo elgis mapserver.x86_64; \
-yumdownloader --resolve --enablerepo epel fribidi.x86_64; \
-yumdownloader --resolve --enablerepo pgdg91 postgresql91.x86_64; \
-yumdownloader --resolve --enablerepo pgdg91 postgresql91-devel.x86_64; \
-yumdownloader --resolve --enablerepo pgdg91 postgresql91-server.x86_64; \
-yumdownloader --resolve --enablerepo pgdg91 postgresql91-docs.x86_64; \
-yumdownloader --resolve --enablerepo pgdg91 postgresql91-python.x86_64; \
-yumdownloader --resolve --enablerepo base uuid.x86_64; \
-yumdownloader --resolve --enablerepo pgdg91 postgresql91-contrib.x86_64; \
-yumdownloader --resolve --enablerepo pgdg91 postgresql91-test.x86_64; \
-yumdownloader --enablerepo pgdg91 pgbouncer.x86_64; \
-yumdownloader --resolve --enablerepo epel json-c.x86_64; \
-yumdownloader --resolve --enablerepo pgdg91 postgis2_91.x86_64; \
-yumdownloader --resolve --enablerepo rpmforge hdf4-devel.x86_64; \
-yumdownloader --resolve --enablerepo rpmforge hdf5-devel.x86_64; \
-yumdownloader --resolve --enablerepo base readline-devel.x86_64; \
-yumdownloader --resolve --enablerepo base giflib-devel.x86_64; \
-yumdownloader --resolve --enablerepo base byacc.x86_64; \
-yumdownloader --resolve --enablerepo base subversion.x86_64; \
-yumdownloader --resolve --enablerepo base cmake.x86_64; \
-yumdownloader --resolve --enablerepo base screen.x86_64; \
-)
+#do this only once for roll distro to keep known RPMS in the roll src
+#(cd src/RPMS; 
+#wget ftp://ftp.pbone.net/mirror/atrpms.net/el6-x86_64/atrpms/stable/bitstream-vera-sans-fonts-1.10-18.el6.noarch.rpm; \
+#wget ftp://ftp.pbone.net/mirror/atrpms.net/el6-i386/atrpms/stable/bitstream-vera-fonts-common-1.10-18.el6.noarch.rpm; \
+#yumdownloader --resolve --enablerepo elgis mapserver.x86_64; \
+#yumdownloader --resolve --enablerepo epel fribidi.x86_64; \
+#yumdownloader --resolve --enablerepo pgdg91 postgresql91.x86_64; \
+#yumdownloader --resolve --enablerepo pgdg91 postgresql91-devel.x86_64; \
+#yumdownloader --resolve --enablerepo pgdg91 postgresql91-server.x86_64; \
+#yumdownloader --resolve --enablerepo pgdg91 postgresql91-docs.x86_64; \
+#yumdownloader --resolve --enablerepo pgdg91 postgresql91-python.x86_64; \
+#yumdownloader --resolve --enablerepo base uuid.x86_64; \
+#yumdownloader --resolve --enablerepo pgdg91 postgresql91-contrib.x86_64; \
+#yumdownloader --resolve --enablerepo pgdg91 postgresql91-test.x86_64; \
+#yumdownloader --enablerepo pgdg91 pgbouncer.x86_64; \
+#yumdownloader --resolve --enablerepo epel json-c.x86_64; \
+#yumdownloader --resolve --enablerepo pgdg91 postgis2_91.x86_64; \
+#yumdownloader --resolve --enablerepo rpmforge hdf4.x86_64 hdf4-devel.x86_64; \
+#yumdownloader --resolve --enablerepo rpmforge hdf5.x86_64 hdf5-devel.x86_64; \
+#yumdownloader --resolve --enablerepo base readline-devel.x86_64; \
+#yumdownloader --resolve --enablerepo base giflib-devel.x86_64; \
+#yumdownloader --resolve --enablerepo base byacc.x86_64; \
+#yumdownloader --resolve --enablerepo base neon.x86_64 pakchois.x86_64 subversion.x86_64; \
+#yumdownloader --resolve --enablerepo base cmake.x86_64; \
+#yumdownloader --resolve --enablerepo base screen.x86_64; \
+#)
 
 # add dynamic libs
 echo "/usr/java/latest/jre/lib/amd64" > /etc/ld.so.conf.d/lifemapper-server.conf
@@ -60,6 +62,7 @@ yum --enablerepo base install subversion
 # for mapserver
 rpm -i src/RPMS/bitstream-vera-fonts-common-1.10-18.el6.noarch.rpm
 rpm -i src/RPMS/bitstream-vera-sans-fonts-1.10-18.el6.noarch.rpm
+
 
 # for mysql-python, rtree, pylucene
 compile setuptools
