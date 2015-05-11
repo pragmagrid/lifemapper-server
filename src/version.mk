11dba87101786418d853863274bad19fc835b509
Aimee Stewart <zzeppozz@gmail.com>
2015-05-11 17:52:18 -0500

    register compute resource with LmServer

--- src/version.mk
+++ src/version.mk
@@ -10,9 +10,22 @@ LMDISK        = /share/lm
 UNIXSOCKET    = /var/run/postgresql
 SMTPSERVER    = localhost
 SMTPSENDER    = no-reply-lifemapper@@PUBLIC_FQDN@
-LMUSER        = idigbio
-DATASOURCE            = IDIGBIO
-ALGORITHMS            = ATT_MAXENT
-MODEL_SCENARIO        = WC-5MIN
-PROJECTION_SCENARIOS  = RCP8.5-CCSM4-2050,RCP8.5-CCSM4-2070
 
+LMUSER                = lm2
+DATASOURCE            = GBIF
+ALGORITHMS            = BIOCLIM, GARP_BS, ATT_MAXENT
+MODEL_SCENARIO        = CRU_CL_2.0_x_hadley_diff
+PROJECTION_SCENARIOS  = hadley_diff_a1f_x_CRU_CL_2.0, hadley_diff_a2c_x_CRU_CL_2.0, hadley_diff_b1a_x_CRU_CL_2.0
+
+COMPUTE_CONTACT_ID    = aimee.stewart@ku.edu
+COMPUTE_CONTACT_EMAIL = aimee.stewart@ku.edu
+COMPUTE_CONTACT_PASS  = whatever
+COMPUTE_CONTACT_FIRST = Aimee
+COMPUTE_CONTACT_LAST  = Stewart
+COMPUTE_CONTACT_INST  = KUBI
+COMPUTE_CONTACT_ADDR1 = 1345 Jayhawk Blvd
+COMPUTE_CONTACT_ADDR2 = Lawrence, KS 66045
+COMPUTE_CONTACT_ADDR3 = USA
+
+COMPUTE_NAME           = BIRC
+COMPUTE_FQDN           = 129.237.201.247
