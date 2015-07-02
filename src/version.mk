1dcc797943c97c5443ead2c77f84c9dba587a103
zzeppozz <aimee.stewart@ku.edu>
2015-07-02 11:32:00 -0500

    Moved site-specific variables to site-specific config file in source code

--- src/version.mk
+++ src/version.mk
@@ -9,17 +9,3 @@ PGSQLVER      = 9.1
 LMDISK        = /share/lm
 UNIXSOCKET    = /var/run/postgresql
 SMTPSERVER    = localhost
-
-DB_FQDN       = hera.nhm.ku.edu
-WEB_FQDN      = hermes.nhm.ku.edu
-PUBLIC_FQDN   = lifemapper.org
-SERVICE_FQDN  = svc.lifemapper.org
-
-LMUSER                = bison
-DATASOURCE            = BISON
-ALGORITHMS            = ATT_MAXENT
-MODEL_SCENARIO        = WC-CONUS 
-PROJECTION_SCENARIOS  = A1B-MIROC3.2-Med-1039-CONUS, A1B-MIROC3.2-Med-4069-CONUS, A1B-MIROC3.2-Med-7099-CONUS
-                        A2-MIROC3.2-Med-1039-CONUS, A2-MIROC3.2-Med-4069-CONUS, A2-MIROC3.2-Med-7099-CONUS
-                        B1-MIROC3.2-Med-1039-CONUS, B1-MIROC3.2-Med-4069-CONUS, B1-MIROC3.2-Med-7099-CONUS
-SCENARIO_PACKAGE      = CONUS_WC_AR4
