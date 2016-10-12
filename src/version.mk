ba1816a08a4a0b8bc5a8acb269121fbd6754d534
Aimee Stewart <zzeppozz@gmail.com>
2016-10-12 17:23:19 -0500

    update code version

--- src/version.mk
+++ src/version.mk
@@ -3,15 +3,17 @@ LMHOME        = /opt/lifemapper
 MAPSERVER_TMP = /var/www/tmp
 LMWEB         = /var/lib/lm2
 LMURL			  = http://lifemapper.org/dl
-DATADIR_SERVER  = /share/lmserver/data
-
-# This variable is identical in the lifemapper-server roll
+LMSERVERDISK  = /share/lmserver
+LMDISK        = /share/lm
+## DATADIR_SHARED is identical in the lifemapper-compute roll
 DATADIR_SHARED  = /share/lm/data
-
-# This variable is identical to INPUT_LAYER_DIR in the lifemapper-compute roll
-ENV_DATA_DIR  = layers
-TEMPDIR       = /tmp
+DATADIR_SERVER  = /share/lmserver/data
 LMSCRATCHDISK = /state/partition1/lmscratch
+TEMPDIR       = /tmp
+
+## ENV_DATA_DIR matches INPUT_LAYER_DIR in the lifemapper-compute roll
+ENV_DATA_DIR  = layers
+
 PYTHONVER     = python2.7
 PGSQLVER      = 9.1
 UNIXSOCKET    = /var/run/postgresql
@@ -19,7 +21,9 @@ SMTPSERVER    = localhost
 SMTPSENDER    = no-reply-lifemapper@@PUBLIC_FQDN@
 JAVABIN       = /usr/java/latest/bin/java
 
-SPECIES_DATA 	       = sorted_seasia_gbif
+# SCENARIO_PACKAGE matches SCENARIO_PACKAGE_SEED in the lifemapper-compute roll
 SCENARIO_PACKAGE      = 10min-past-present-future
+
+SPECIES_DATA 	       = sorted_seasia_gbif
 GRID_NAME             = lmgrid_1d
 GRID_CELLSIZE         = 1
