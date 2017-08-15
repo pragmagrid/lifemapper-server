23f996242514b5ad7b4b21fb23a83cb3fc64851c
Aimee Stewart <zzeppozz@gmail.com>
2017-08-15 13:08:09 -0500

    bugfixes

--- src/version.mk
+++ src/version.mk
@@ -9,8 +9,8 @@ DATADIR_SHARED  = /share/lm/data
 DATADIR_SERVER  = /share/lmserver/data
 LMSCRATCHDISK = /state/partition1/lmscratch
 
-## ENV_DATA_DIR matches INPUT_LAYER_DIR in the lifemapper-compute roll
-ENV_DATA_DIR  = layers
+## Matches ENV_DATA_DIR in the lifemapper-compute roll
+ENV_DATA_DIR   = layers
 
 PYTHONVER     = python2.7
 PGSQLVER      = 9.1
