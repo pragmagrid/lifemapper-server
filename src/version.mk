d7c3bd75d62deaac5f1c2f6f34f1c402831ceae3
zzeppozz <zzeppozz@gmail.com>
2019-12-19 10:21:50 -0600

    codeversions

--- src/version.mk
+++ src/version.mk
@@ -49,8 +49,8 @@ GRID_CELLSIZE			= 1
 GRID_NUM_SIDES			= 4
 
 # Code version
-LMCODE_VERSION			  = 3.3.4
-LMVIZ_VERSION            = 2.2.15
+LMCODE_VERSION			  = 3.3.5
+LMVIZ_VERSION            = 2.3.1
 
 
 
